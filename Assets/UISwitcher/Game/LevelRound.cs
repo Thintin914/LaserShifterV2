63a4121eef2732a50b3ef23441e783c42b83e9bf
JackyWu <ttca914@gmail.com>
2022-03-21 02:42:49 +0800

    fix

--- Assets/UISwitcher/Game/LevelRound.cs
+++ Assets/UISwitcher/Game/LevelRound.cs
@@ -72,7 +72,6 @@ public class LevelRound : MonoBehaviour
 
     public async void InitalizeLevel()
     {
-        GameUI.Instance.player.pv.RPC("ChangeUsername", RpcTarget.All, CommonUI.Instance.username);
         if (isHost)
         {
             startTime = Time.deltaTime;
